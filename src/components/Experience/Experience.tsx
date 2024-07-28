66daa36cc56e3d064187fb980a28e9f164eaa27b
it21263576 <it21263576@my.sliit.lk>
2024-07-29 00:20:50 +0530

    content and experience pages

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -17,61 +17,61 @@ import Icon3D2 from '/public/Images/3D2.jpg'
 
 const Experience = () => {
   return (
-    <div className="mt-16 p-32 flex flex-wrap text-white justify-between items-center">
-      <div className="mb-32">
-        <h4 className='languages-t text-xs w-56 font-semibold border border-white rounded-full py-1 ml-10 pl-5 mb-1'>Web Development & Languages</h4>
-        <h2 className="pt-2 text-3xl font-bold">I'm Certified & Skilled Developer</h2>
-        <p className='pt-5'>👋 Hi there! I'm a web developer passionate about crafting beautiful
+    <div className="p-8 justify-center 2xl:mt-16 2xl:p-32 flex flex-wrap text-white 2xl:justify-between items-center">
+      <div className="mb-10 2xl:mb-32">
+        <h4 className='languages-t text-custom-xs ml-10 w-40 font-semibold border border-white rounded-full py-1 pl-5 mb-1 2xl:text-xs 2xl:w-56 2xl:ml-10'>Web Development & Languages</h4>
+        <h2 className="pt-2 text-sm 2xl:text-3xl font-bold">I'm Certified & Skilled Developer</h2>
+        <p className='pt-5 text-sm 2xl:text-base'>👋 Hi there! I'm a web developer passionate about crafting beautiful
           and functional<br/> web experiences. With a strong foundation in both front-end
           and back-end technologies,<br/> I love turning ideas into real-world applications.</p>
       </div>
 
-      <div className="flex flex-col mb-20 ml-36">
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+      <div className="gap-1 2xl:gap-0 flex 2xl:flex-col 2xl:mb-20 2xl:ml-36">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={htmlIcon.src} alt="HTML"/>HTML
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg font-bold p-3 mb-2 2xl:text-custom-sm 2xl:w-36 2xl:p-2">
           <img className="w-7 inline mr-2" src={cssIcon.src} alt="CSS"/>CSS
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={jsIcon.src} alt="JavaScript"/>JavaScript
         </h4>
       </div>
 
-      <div className="flex flex-col mb-20">
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+      <div className="gap-1 flex 2xl:gap-0 2xl:flex-col 2xl:mb-20">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={reactIcon.src} alt="React"/>REACT
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={nextjsIcon.src} alt="Next.js"/>NEXT.JS
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={vuejsIcon.src} alt="Vue.js"/>VUE.JS
         </h4>
       </div>
 
-      <div className="flex flex-col mb-20">
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+      <div className="gap-1 flex 2xl:gap-0 2xl:flex-col mb-20">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={nodejsIcon.src} alt="Node.js"/>node.js
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-custom-p mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={springIcon.src} alt="Spring Boot"/>Springboot
         </h4>
-        <h4 className="text-center bg-white text-black rounded-lg w-36 font-bold p-2 mb-2">
+        <h4 className="w-20 text-center text-xs bg-white text-black rounded-lg 2xl:w-36 font-bold p-2 mb-2 2xl:text-custom-sm">
           <img className="w-7 inline mr-2" src={bootstrapIcon.src} alt="Bootstrap"/>Bootstrap
         </h4>
       </div>
 
       <div className="flex flex-wrap text-white justify-between items-center gap-18">
-        <div className="mb-40">
-          <h4 className='languages-t text-xs w-48 font-semibold border border-white rounded-full py-1 ml-10 pl-5 mb-1'>3D Modeling & Animation</h4>
-          <h2 className="pt-2 text-3xl font-bold">I'm Certified & Skilled 3D Designer</h2>
-          <p className='pt-5'>I discovered my love for 3D design 5 years ago and have since
+        <div className="mb-10 2xl:mb-40">
+          <h4 className='languages-t text-custom-xs 2xl:text-xs w-36 font-semibold border border-white rounded-full py-1 ml-10 pl-5 mb-1 2xl:w-48'>3D Modeling & Animation</h4>
+          <h2 className="pt-2 text-sm 2xl:text-3xl font-bold">I'm Certified & Skilled 3D Designer</h2>
+          <p className='pt-5 text-sm 2xl:text-base'>I discovered my love for 3D design 5 years ago and have since
             honed my skills<br/> in creating immersive and visually captivating 3D
             models, animations, and environments.<br/> My work spans across various industries, including gaming, film, architecture, and product<br/> design.</p>
         </div>
         <div>
-          <img className="mb-32 ml-20" src={Icon3D2.src} alt="3D2" width="500px"/>
+          <img className="mb-32 2xl:ml-20" src={Icon3D2.src} alt="3D2" width="500px"/>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,11 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontSize: {
         'custom-xs': '8px',
+        'custom-sm': '14px',
       },
       colors: {
         customGray: '#4e4e4e',
@@ -22,8 +22,12 @@ const config: Config = {
       boxShadow: {
         'custom-shadow': '0 .3rem .1rem rgba(0, 0, 0, .2)',
       },
+      padding: {
+        'custom-p': '14px',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
