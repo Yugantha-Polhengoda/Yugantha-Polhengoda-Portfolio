5ecbda865d27108537ef32f7d6c2ec9edfa17158
it21263576 <it21263576@my.sliit.lk>
2024-07-31 22:54:50 +0530

    type writter effect

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,9 @@
+"use client"; 
+
 import React from 'react'
 import Image from "next/image";
 import ProfileImg from '/public/Images/Adobe6.png'
+import { Typewriter } from 'react-simple-typewriter';
 import './Hero.css'
 
 import { FaUser, FaEnvelope, FaPhoneAlt } from 'react-icons/fa'; // Import icons from react-icons
@@ -28,9 +31,18 @@ const Hero = () => {
 
           <Image src={ProfileImg} alt="Profile Image" className=' w-52 ml-9 md:w-52 md:h-52 md:ml-8 lg:w-72 lg:h-72 lg:ml-0 2xl:w-72 2xl:ml-0'/>
             <div className='mt-4 lg:absolute md:ml-5 md:mt-5 lg:ml-80 lg:mt-14 2xl:absolute 2xl:ml-80 2xl:mt-12'>
-                <h3 className="text-white md:pb-1 md:text-xl lg:pb-1 lg:text-2xl 2xl:pb-1 2xl:text-2xl">Hello, It's Me</h3>
-                <h1 className="text-white text-lg pb-1 font-semibold md:text-2xl lg:text-4xl 2xl:text-4xl">Yugantha Polhengoda</h1>
-                <h3 className="text-white text-sm mb-4 md:text-xl lg:text-2xl 2xl:text-2xl">And I'm a <span className='ml-1' id="text">Web Developer...</span></h3> 
+                <h3 className="text-white font-medium md:pb-1 md:text-xl lg:pb-1 lg:text-2xl 2xl:pb-1 2xl:text-2xl">Hello, It's Me</h3>
+                <h1 className="text-white text-lg pb-1 font-semibold md:text-2xl lg:text-4xl 2xl:pb-2 2xl:text-4xl">Yugantha Polhengoda</h1>
+                <h3 className="text-white font-medium text-sm mb-4 md:text-xl lg:text-2xl 2xl:text-2xl">And I'm a 
+                  <span className='ml-2' id="text"><Typewriter
+                      words={['Frontend Developer...', 'Backend Developer...', 'UI/UX Designer...', '3D Artist...']}
+                      loop={true}
+                      cursor
+                      cursorStyle='_'
+                      typeSpeed={70}
+                      deleteSpeed={50}
+                      delaySpeed={1000}
+                    /></span></h3> 
                 
                 <div className="flex gap-3 2xl:mt-5">
                     <button className="inline-flex items-center px-6 py-2 border border-transparent text-sm 2xl:text-lg font-medium rounded-md shadow-sm text-black bg-white hover:bg-gray-200 group">
