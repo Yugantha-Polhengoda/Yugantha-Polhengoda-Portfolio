009a00b8846320be68a072b87391aababe53a2e2
it21263576 <it21263576@my.sliit.lk>
2024-08-26 14:38:45 +0530

    phone number added to the button

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,14 +6,8 @@ import ProfileImg from '/public/Images/Adobe6.png'
 import { Typewriter } from 'react-simple-typewriter';
 import './Hero.css'
 
-import { FaUser, FaEnvelope, FaPhoneAlt } from 'react-icons/fa'; // Import icons from react-icons
-import { FaBriefcase } from "react-icons/fa6";
-import { HiOutlineBriefcase } from "react-icons/hi2";
-import { LiaPhoneAltSolid } from "react-icons/lia";
-import { MdSimCardDownload } from "react-icons/md";
-import { LiaFileDownloadSolid } from "react-icons/lia";
-import { FaCode } from 'react-icons/fa';
 import { FaDownload } from "react-icons/fa";
+import { LiaPhoneAltSolid } from "react-icons/lia";
 
 const Hero = () => {
   return (
@@ -33,7 +27,7 @@ const Hero = () => {
                         2xl:text-left 2xl:inline-flex 2xl:w-72 2xl:h-72 2xl:ml-20 ">
 
           <Image src={ProfileImg} alt="Profile Image" className=' w-52 ml-9 md:w-52 md:h-52 md:ml-8 lg:w-72 lg:h-72 lg:ml-0 2xl:w-72 2xl:ml-0'/>
-            <div className="mt-4 lg:absolute md:ml-5 md:mt-5 lg:ml-80 lg:mt-14 2xl:absolute 2xl:ml-80 2xl:mt-12">
+            <div className="mt-4 lg:absolute md:ml-5 md:mt-5 lg:ml-80 lg:mt-14 2xl:ml-80 2xl:mt-12">
                 <h3 className="text-white font-medium md:pb-1 md:text-xl lg:pb-1 lg:text-2xl 2xl:pb-1 2xl:text-2xl">Hello, It's Me</h3>
                 <h1 className="text-white text-lg pb-1 font-semibold md:text-2xl lg:text-4xl 2xl:pb-2 2xl:text-4xl">Yugantha Polhengoda</h1>
                 <h3 className="text-white font-medium text-sm mb-4 md:text-xl lg:text-2xl 2xl:text-2xl">And I'm a 
@@ -53,8 +47,7 @@ const Hero = () => {
                         Resume
                     </a>
 
-
-                    <a href="#Contact"className="inline-flex items-center px-6 py-2 border border-white text-sm 2xl:text-lg font-medium rounded-md shadow-sm text-white bg-transparent hover:bg-white hover:text-zinc-800 group">
+                    <a href="tel:+94767801452" className="inline-flex items-center px-6 py-2 border border-white text-sm 2xl:text-lg font-medium rounded-md shadow-sm text-white bg-transparent hover:bg-white hover:text-zinc-800 group">
                     <LiaPhoneAltSolid size={25} className="phone-Icon mr-1" />
                         Contact
                     </a>
@@ -66,6 +59,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
-
-
+export default Hero;
