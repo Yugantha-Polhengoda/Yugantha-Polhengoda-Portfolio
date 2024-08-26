152eccf920252db5124bc42b262982b6cd5765e8
it21263576 <it21263576@my.sliit.lk>
2024-08-26 15:56:26 +0530

    customer service

--- src/components/CustomerService/CustomerService.tsx
+++ src/components/CustomerService/CustomerService.tsx
@@ -6,7 +6,7 @@ import customerServiceIcon from '/public/Images/customer-service.png'
 const CustomerService = () => {
   return (
     <div>
-      <a href='#Contact' className="fixed z-40 right-4 bottom-10 2xl:pr-3 pb-3 text-white rounded-full shadow-lg transition-transform transform hover:scale-110">
+      <a href="tel:+94767801452" className="fixed z-40 right-4 bottom-10 2xl:pr-3 pb-3 text-white rounded-full shadow-lg transition-transform transform hover:scale-110">
         <Image className="w-10 2xl:w-20 ml-2" src={customerServiceIcon.src} alt="customerService" width={80} height={0}/><span className='text-custom-xs 2xl:text-lg'>Contact me</span>
       </a>
     </div>
